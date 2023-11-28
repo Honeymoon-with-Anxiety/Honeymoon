667732b693fe18cd9f7be1206d44f2872276b2b3
Písek Pískovec <pisekpiskovec@gmail.com>
2023-11-28 22:26:10 +0100

    Clean

--- cmakeQuickStart/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /run/media/pisek/Společné/E3A/cmakeQuickStart
-# Build directory: /run/media/pisek/Společné/E3A/cmakeQuickStart/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
