ae1b2c9f111a3fb85f4b6a3879b6b3e0440fc6d9
Písek Pískovec <77384152+pisekpiskovec@users.noreply.github.com>
2025-02-07 10:14:26 +0100

    MIT_7_2_25

--- MIT/6_12_24/6_12_24.asm
+++ MIT/6_12_24/6_12_24.asm
@@ -1,11 +1,58 @@
 .include "templateM16.inc"
+.include "makra.inc"
+.include "display.inc"
+
+;====== Register definitions =======
+;ZeroReg = r0
+;ADCReg = r16
+;MaskReg = r17
+;Cislo = r18
+;TmpReg = r19
+;number = r20
+;PDelReg = r21
+;PDelReg0 = r22
+;PDelReg2 = r23
+;PDelReg1 = r24
 
 .DSEG
 disp_RAM: .byte 4
 .CSEG
 
 Main:
+	clr number
+	in ADCReg, ADCH
 	
+	adiw XL, 1
+	adc XH, ZeroReg
+	
+	mov r26, XH
+	mov r27, XL
+	
+	rcall initDisplay
+	rcall rozeberNaCislice
+	rcall initZCharTable
+	
+	loop:
+		in number, ADCH
+		rcall rozeberNaCislice
+		
+		ldd selectedChar, Y+0
+		ldi PDelReg, 5
+		rcall Delay1m
+		digit_hundreds_ON
+		rcall zobrazovaciPodprogram
+		
+		ldd selectedChar, Y+1
+		ldi PDelReg, 5
+		rcall Delay1m
+		digit_tens_ON
+		rcall zobrazovaciPodprogram
+
+		ldd selectedChar, Y+2
+		ldi PDelReg, 5
+		rcall Delay1m
+		digit_units_ON
+		rcall zobrazovaciPodprogram
 rjmp Main
 
 display:
